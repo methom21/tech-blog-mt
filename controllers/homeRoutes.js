83f3b97108004bbf85f360edc4d54102e2b215ab
methom21 <mikey.thompson8@gmail.com>
2021-08-03 11:18:53 -0700

    undid the comment out

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,27 +27,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/post/:id', async (req, res) => {
-//   try {
-//     const postData = await Post.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-//     const post = postData.get({ plain: true });
+    const post = postData.get({ plain: true });
 
-//     res.render('post', {
-//       ...post,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
